5bc2db48906e09702cd6b5eaf33cca8751681456
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-09-29 14:19:26 -0400

    A lil format push

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,57 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos")
-}
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
-getUserRepos();
+var displayRepos = function (repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
+  // clear old content
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+  // loop over repos
+  for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+    // create a container for each repo
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
+    // append to container
+    repoEl.appendChild(titleEl);
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+  console.log(event);
+};
+
+var getUserRepos = function (user) {
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  fetch(apiUrl).then(function (response) {
+    console.log(response);
+    response.json().then(function (data) {
+      displayRepos(data, user);
+    });
+  });
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
