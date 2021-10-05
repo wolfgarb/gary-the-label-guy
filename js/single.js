7a4b86a3e6abe88aeb7222f0425ad217fad4e2d3
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-10-05 12:15:39 -0400

    Get headers for links

--- js/single.js
+++ js/single.js
@@ -1,56 +1,74 @@
-// this var is a reference to the DOM
+// this var is a reference to the DOM that holds repo issues
 var issuesContainer = document.querySelector("#issues-container");
+// this DOM references limit warning function
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See more issues on GitHub";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append all created elements to warning container
+  limitWarningEl.appendChild(linkEl);
+};
 
 var getRepoIssues = function(repo) {
-    console.log(repo);
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                displayIssues(data);
-            });
-        } else {
-            alert("Error with request");
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayIssues(data);
+
+        // check if API has paginated issues (more than 30 wont be displayed, link to github)
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
         }
-    });
+      });
+    } else {
+      alert("Error with request");
+    }
+  });
 };
 
 var displayIssues = function(issues) {
-    if (issues.length === 0) {
-        issuesContainer.textContent = "This repo has no open issues.";
-        return;
+  if (issues.length === 0) {
+    issuesContainer.textContent = "This repo has no open issues.";
+    return;
+  }
+  for (var i = 0; i < issues.length; i++) {
+    // create a link element to take users to the issue on github
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+
+    // span el to hold issue TITLE
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+
+    // append to the issueEl container
+    issueEl.appendChild(titleEl);
+
+    // create el for issue TYPE
+    var typeEl = document.createElement("span");
+
+    // check if issue or pull request
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    } else {
+      typeEl.textContent = "(Issue)";
     }
-    for (var i = 0; i < issues.length; i++) {
-        // create a link element to take users to the issue on github
-        var issueEl = document.createElement("a");
-        issueEl.classList = "list-item flex-row justify-space-between align-center";
-        issueEl.setAttribute("href", issues[i].html_url);
-        issueEl.setAttribute("target", "_blank");
 
-        // span el to hold issue TITLE
-        var titleEl = document.createElement("span");
-        titleEl.textContent = issues[i].title;
+    // append to the issueEl container
+    issueEl.appendChild(typeEl);
 
-        // append to the issueEl container
-        issueEl.appendChild(titleEl);
+    //append all created elements to the DOm
+    issuesContainer.appendChild(issueEl);
+  }
+};
 
-        // create el for issue TYPE
-        var typeEl = document.createElement("span");
-
-        // check if issue or pull request
-        if (issues[i].pull_request) {
-            typeEl.textContent = "(Pull request)";
-        } else {
-            typeEl.textContent = "(Issue)";
-        }
-
-        // append to the issueEl container
-        issueEl.appendChild(typeEl);
-
-        //append all created elements to the DOm
-        issuesContainer.appendChild(issueEl);
-    }
-}
-
-getRepoIssues("wolfgarb/wolfgarb");
+getRepoIssues("facebook/react");
